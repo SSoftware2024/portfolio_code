0e017d481b8d5d2cc1ffeb95813dae8c6f9ba521
Soluções Software <ssolucoessoftware@gmail.com>
2024-03-14 19:43:05 -0300

    certo

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -4,7 +4,7 @@
         <div class="w-full 2xl:max-h-[700px] sm:max-h-[500px]">
             <div class="flex flex-col justify-center items-center space-y-2">
                 <CardProject class="w-[80%] px-2 py-2 bg-[#000419]" :img="images.projects.portfolio" title="Portfolio" github="https://github.com/SSoftware2024/portfolio-code" 
-                online="https://github.com/SSoftware2024/portfoliopcode">
+                online="https://github.com/SSoftware2024/portfolio_code">
                     Meu portfolio, projeto para exibir um pouco sobre minha pessoa como profissional, minhas habilidades
                     e projetos públicos realizados.
                     Esse incluise foi feito com:
